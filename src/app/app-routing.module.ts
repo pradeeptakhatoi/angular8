dcf8eaca675c47972950e048b3ff2d193de5b506
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-01-10 17:14:30 +0530

    Theme Integrated & New pages added with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListUsersComponent } from './users/list/list.component';
+import { AddUserComponent } from './users/add/add.component';
+import { PageNotFoundComponent } from './not-found/not-found.component';
+import { SiteLayoutComponent } from './site-layout/site-layout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Site routes goes here 
+  {
+    path: '',
+    component: SiteLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'users', component: ListUsersComponent },
+      { path: 'add-user', component: AddUserComponent },
+      { path: 'aboutus', component: AboutusComponent },
+    ]
+  },
+  //no layout routes
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' },  
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUsersComponent } from './users/list/list.component';
+import { AddUserComponent } from './users/add/add.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './not-found/not-found.component';
+import { SiteLayoutComponent } from './site-layout/site-layout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUsersComponent,
+    AddUserComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    SiteLayoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
